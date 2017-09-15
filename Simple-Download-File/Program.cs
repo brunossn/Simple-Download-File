aef8a879e9c1457837d1427bf6a02d46e65e040b
Bruno Soares <brunossn+coding@gmail.com>
2017-09-15 15:09:17 -0300

    Commit inicial

---
+++ Simple-Download-File/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Download_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(ValidateArgs(args))
+            {
+
+            }
+        }
+
+        static bool ValidateArgs(string[] args)
+        {
+            return true;
+        }
+    }
+}
